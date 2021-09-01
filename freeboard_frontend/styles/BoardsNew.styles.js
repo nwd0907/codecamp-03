f832317d48ddc4d16cfd3ff610167bb33b6deca7
nwd0907 <nwd0907>
2021-09-01 09:43:39 +0900

    Update state-error

--- freeboard_frontend/styles/BoardsNew.styles.js
+++ freeboard_frontend/styles/BoardsNew.styles.js
@@ -166,3 +166,9 @@ export const SubmitButton = styled.button`
 	margin-right: 12px;
 	cursor: pointer;
 `;
+
+export const Error = styled.div`
+	padding-top: 10px;
+	font-size: 14px;
+	color: red;
+`
