c16d43ed3cee73a186e9ff86e1ca8188fb8105cf
nwd0907 <nwd0907>
2021-09-02 16:38:56 +0900

    Update rest, graphql

---
+++ class/pages/04-05-graphql-mutation-product/index.js
@@ -0,0 +1,42 @@
+import { useMutation, gql } from "@apollo/client"
+
+const CREATE_PRODUCT = gql`
+    mutation createProduct($seller: String, $createProductInput: CreateProductInput!) {
+        createProduct(
+            seller: $seller, 
+            createProductInput: $createProductInput
+        ){
+            _id
+            number
+            message
+        }
+    }
+`
+
+export default function GraphqlMutationProductPage(){
+    const [ createProduct ] = useMutation(CREATE_PRODUCT)
+
+    async function onClickSubmit(){
+        const result = await createProduct({
+            variables: {
+                seller: "영희",
+                createProductInput: {
+                    name: "키보드",
+                    detail: "아주 좋은 키보드",
+                    price: 5000
+                }
+            }
+        })
+        console.log(result.data.createProduct._id)
+    }
+
+    return (
+        <>
+            판매자: <input type="text" />
+            상품명: <input type="text" />
+            상품상세: <input type="text" />
+            상품가격: <input type="text" />
+            <button onClick={onClickSubmit}>상품 등록하기!!</button>
+        </>
+    )
+}
