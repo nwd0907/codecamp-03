59cc06df0a6f657afe9f57bc1a800726ac09141a
nwd0907 <nwd0907@newbizstart.com>
2021-10-26 13:01:17 +0900

    build-ssr

--- class_build/pages/boards/index.js
+++ class_build/pages/boards/index.js
@@ -1,3 +1,17 @@
+import Head from "next/head";
+
 export default function BoardsPage() {
-  return <div>게시판입니다!</div>;
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="나의사이트 게시판페이지" />
+        <meta property="og:description" content="환영합니다!!" />
+        <meta
+          property="og:image"
+          content="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
+        />
+      </Head>
+      <div>게시판입니다!</div>
+    </>
+  );
 }
